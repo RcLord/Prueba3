6ab66a20dad03f6a167150454c9c9e13ac2c64ae
rsalazar <rsalazar@sistemas2.triveca.local>
2019-09-30 10:37:35 -0500

    Primer commit desde Eclipse

---
+++ Prueba3/src/main/java/com/ejemplo/Prueba3/Prueba3Application.java
@@ -0,0 +1,15 @@
+package com.ejemplo.Prueba3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba3Application.class, args);
+		System.out.println("hola mundo 3");
+		
+	}
+
+}
---
+++ Prueba3/src/main/java/com/ejemplo/Prueba3/entity/Role.java
@@ -0,0 +1,59 @@
+package com.ejemplo.Prueba3.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Role implements Serializable{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4575386223294270528L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+
+}
